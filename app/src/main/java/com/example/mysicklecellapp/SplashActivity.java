68c1e1e5e42c818370dba49a2b79240164931231
Afeez <Afeezoluwa@yahoo.ie>
2024-04-13 18:03:18 +0100

    Changed DashBoard ,Addes Drawables needed and added splash screen update

---
+++ app/src/main/java/com/example/mysicklecellapp/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.mysicklecellapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    // Set the duration of the splash screen
+    private static final long SPLASH_DELAY = 2000; // 2 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash); // Create a layout file for your splash screen if not already done
+
+        // Use a handler to delay the start of the main activity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Start the main activity after the splash delay
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish(); // Close the splash activity so that the user cannot go back to it
+            }
+        }, SPLASH_DELAY);
+    }
+}
